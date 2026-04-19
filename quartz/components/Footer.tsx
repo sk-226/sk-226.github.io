bb2b3198de98c4ca9ac67a7c6bf2d7a6022decf8
sk-226 <skurita0208@gmail.com>
2026-04-19 17:52:17 +0900

    不要なfooterを削除

--- quartz/components/Footer.tsx
+++ quartz/components/Footer.tsx
@@ -4,19 +4,23 @@ import { version } from "../../package.json"
 import { i18n } from "../i18n"
 
 interface Options {
-  links: Record<string, string>
+  links?: Record<string, string>
+  showQuartzCredit?: boolean
 }
 
 export default ((opts?: Options) => {
   const Footer: QuartzComponent = ({ displayClass, cfg }: QuartzComponentProps) => {
     const year = new Date().getFullYear()
-    const links = opts?.links ?? []
+    const links = opts?.links ?? {}
+    const showQuartzCredit = opts?.showQuartzCredit ?? false
     return (
       <footer class={`${displayClass ?? ""}`}>
-        <p>
-          {i18n(cfg.locale).components.footer.createdWith}{" "}
-          <a href="https://quartz.jzhao.xyz/">Quartz v{version}</a> © {year}
-        </p>
+        {showQuartzCredit ? (
+          <p>
+            {i18n(cfg.locale).components.footer.createdWith}{" "}
+            <a href="https://quartz.jzhao.xyz/">Quartz v{version}</a> © {year}
+          </p>
+        ) : null}
         <ul>
           {Object.entries(links).map(([text, link]) => (
             <li>
