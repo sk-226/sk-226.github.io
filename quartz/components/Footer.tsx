ca641c7206094c809f82625aff0cab777c06c96b
sk-226 <skurita0208@gmail.com>
2026-04-19 18:04:07 +0900

    Remove Quartz credit section

--- quartz/components/Footer.tsx
+++ quartz/components/Footer.tsx
@@ -1,28 +1,23 @@
 import { QuartzComponent, QuartzComponentConstructor, QuartzComponentProps } from "./types"
 import style from "./styles/footer.scss"
-import { version } from "../../package.json"
-import { i18n } from "../i18n"
 
 interface Options {
   links?: Record<string, string>
-  showQuartzCredit?: boolean
 }
 
 export default ((opts?: Options) => {
-  const Footer: QuartzComponent = ({ displayClass, cfg }: QuartzComponentProps) => {
-    const year = new Date().getFullYear()
+  const Footer: QuartzComponent = ({ displayClass }: QuartzComponentProps) => {
     const links = opts?.links ?? {}
-    const showQuartzCredit = opts?.showQuartzCredit ?? false
+    const entries = Object.entries(links)
+
+    if (entries.length === 0) {
+      return null
+    }
+
     return (
       <footer class={`${displayClass ?? ""}`}>
-        {showQuartzCredit ? (
-          <p>
-            {i18n(cfg.locale).components.footer.createdWith}{" "}
-            <a href="https://quartz.jzhao.xyz/">Quartz v{version}</a> © {year}
-          </p>
-        ) : null}
         <ul>
-          {Object.entries(links).map(([text, link]) => (
+          {entries.map(([text, link]) => (
             <li>
               <a href={link}>{text}</a>
             </li>
